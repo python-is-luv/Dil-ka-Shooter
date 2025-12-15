a192481a424e93179bab5cd2322e1e463f58223b
Bhardwaj <bhardwaj.14.mit@gmail.com>
2025-12-15 13:53:25 +0530

    Added script for main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,15 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func ready():
 	pass
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
